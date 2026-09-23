67144c767205dc36c7fb70f97535e051c5bb8ab3
MykeLora <MykeLora@gmail.com>
2026-09-22 22:39:12 -0400

    Adding ef package that was missing to run the migritions.

--- SalesHub.Api/Program.cs
+++ SalesHub.Api/Program.cs
@@ -1,4 +1,6 @@
 
+using SalesHub.Ioc.DependencyInjection;
+
 namespace SalesHub.Api
 {
     public class Program
@@ -8,6 +10,8 @@ namespace SalesHub.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddSalesHubServices(
+                builder.Configuration);
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
