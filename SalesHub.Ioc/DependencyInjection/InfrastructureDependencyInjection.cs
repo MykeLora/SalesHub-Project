e9e675b542162cdc99246b1500bc84397af1e45d
MykeLora <MykeLora@gmail.com>
2026-09-22 22:38:01 -0400

    Registring DependencyInjection and configuring connectionstring.

---
+++ SalesHub.Ioc/DependencyInjection/InfrastructureDependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SalesHub.Infrastructure.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesHub.Ioc.DependencyInjection
+{
+    public static class InfrastructureDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<SalesHubDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
+
+    }
+}
---
+++ SalesHub.Ioc/DependencyInjection/SalesHubDependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesHub.Ioc.DependencyInjection
+{
+    public static class SalesHubDependencyInjection
+    {
+        public static IServiceCollection AddSalesHubServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddInfrastructure(configuration);
+            return services;
+        }
+    }
+}
