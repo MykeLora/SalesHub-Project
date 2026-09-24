85b0762544abd6876419ecbd8fe8f9164266be41
MykeLora <MykeLora@gmail.com>
2026-09-24 00:15:08 -0400

    Interfaces and generic has been implemented.

---
+++ SalesHub.Application/Interface/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using SalesHub.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesHub.Application.Interface.Repositories
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        Task<Product?> GetByCodeAsync(string name);
+        Task<IEnumerable<Product>> GetByCategoryIdAsync(int categoryId);
+        Task<IEnumerable<Product>> GetActiveProductsAsync();
+    }
+}
