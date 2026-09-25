68d1a89d77db224892011a75f49f5ded304bfcc6
MykeLora <MykeLora@gmail.com>
2026-09-25 13:12:27 -0400

    Creating some DTos.

---
+++ SalesHub.Application/DTOs/Product/UpdateProductDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesHub.Application.DTOs.Product
+{
+    public class UpdateProductDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
